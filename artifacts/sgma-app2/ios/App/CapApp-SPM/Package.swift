d4970ac4cb3e7f429aaa263a02404f1f9b2dbe80
omaralharby1989-source <omaralharby1989@gmail.com>
2026-06-20 13:36:25 +0200

    Fix mobile bottom safe area and AdMob banner layout for iOS release build 2

--- artifacts/sgma-app2/ios/App/CapApp-SPM/Package.swift
+++ artifacts/sgma-app2/ios/App/CapApp-SPM/Package.swift
@@ -25,7 +25,8 @@ let package = Package(
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "CapacitorCommunityAdmob", package: "CapacitorCommunityAdmob"),
                 .product(name: "CapacitorSplashScreen", package: "CapacitorSplashScreen")
-            ]
+            ],
+            path: "Sources/Plugin"
         )
     ]
 )
